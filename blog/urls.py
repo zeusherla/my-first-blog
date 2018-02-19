2d474b00582d8d2e78d5b80d5a967097d9fbab6b
zeusherla <zeus.herla@gmail.com>
2018-02-19 19:15:55 +0700

    Extending templates

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
+
+
+# how to insert param? this >> (? what? P? Param? yes, <pk>, in which pk is digits \d+)
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 # Create your views here.
 
 def post_list(request):
-    post2 = Post.objects.order_by('-created_date')
-    return render(request, 'blog/post_list.html', {'posts' : post2})
+    posts = Post.objects.order_by('-created_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+    
+# def post_detail(request, pk):
+    # post = get_object_or_404(Post, pk=pk)
+    # return render(request, 'blog/post_detail.html', {'post': post})
+    
+    
+    
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
